a080593a95d92d28f67a15dfb8d256c2a736d40c
karina <karinaramos0401@gmail.com>
2023-04-18 16:51:11 -0300

    caminho dos assets no nextj

--- next.config.js
+++ next.config.js
@@ -2,11 +2,28 @@
 
 const path = require("path");
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
