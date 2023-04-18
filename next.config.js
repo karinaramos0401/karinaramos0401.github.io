2c37ab8c827bf65d67924e87de2c854035549ee1
karina <karinaramos0401@gmail.com>
2023-04-18 16:27:46 -0300

    ajustando caminho de assets

--- next.config.js
+++ next.config.js
@@ -2,7 +2,21 @@
 
 const path = require("path");
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
