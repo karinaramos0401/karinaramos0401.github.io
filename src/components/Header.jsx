cb3992f227dd3039ad74a072e31570fee409e046
karina <karinaramos0401@gmail.com>
2023-04-18 16:21:47 -0300

    nextjs portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Karina Ramos</h1>
+      <span className="header__subtitle">Product Designer</span>
+    </header>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import HomeView from "@/views/HomeView";
+
+export default function Main() {
+  return <HomeView />;
+}
